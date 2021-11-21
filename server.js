38064c76a91d72cca533c8fae3c68d4a6c4beb24
The Larry <26318510+0xlarry@users.noreply.github.com>
2021-11-21 13:22:25 +0000

    Add server script to facilitate dev

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+app.use(express.static(__dirname));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
